a2067fc250b841b4815129f5e0a501885399c166
Yusuf Molla <mail@yusufmolla.de>
2025-04-19 19:37:29 +0200

    initial commit

--- questions.js
+++ questions.js
@@ -1,86 +1,254 @@
 const questions = {
-    Kulinarik: [
-        { question: "Was ist das Hauptgewürz in der traditionellen griechischen Tsatsiki-Sauce?", answer: "Knoblauch", points: 100 },
-        { question: "Aus welchem Land stammen Nudeln ursprünglich?", answer: "China", points: 200 },
-        { question: "Welche Fleischsorte wird typischerweise für die Zubereitung von Filet Mignon verwendet?", answer: "Rinderfilet", points: 300 },
-        { question: "Was ist das wichtigste Kriterium bei der Herstellung von echtem Parmesan?", answer: "Lange Reifung", points: 400 },
-        { question: "Welches französische Gericht besteht hauptsächlich aus geschichtetem Kartoffelpüree und Sahne?", answer: "Gratin Dauphinois", points: 500 }
-    ],
-    Geografie: [
-        { question: "Welcher Fluss fließt durch London?", answer: "Themse", points: 100 },
-        { question: "Welcher Kontinent hat die meisten Länder?", answer: "Afrika", points: 200 },
-        { question: "Welche Insel ist die größte im Mittelmeer?", answer: "Sizilien", points: 300 },
-        { question: "In welchem Land liegt die Wüste Namib?", answer: "Namibia", points: 400 },
-        { question: "Was ist die Hauptstadt von Bolivien?", answer: "Sucre", points: 500 }
-    ],
-    Anime: [
-        { question: "Wie heißt der Hauptcharakter in 'Bleach'?", answer: "Ichigo Kurosaki", points: 100 },
-        { question: "Welcher Anime spielt in einer Welt, die nach dem ‚Titanfall‘-Verfahren von riesigen, menschenfressenden Wesen beherrscht wird?", answer: "Attack on Titan", points: 200 },
-        { question: "Welche Anime-Serie folgt der Geschichte von vier Freunden, die die Welt retten wollen, indem sie ein mysteriöses Artefakt suchen?", answer: "Fullmetal Alchemist", points: 300 },
-        { question: "Was ist der Name des Pokémon, das in ‚Pokémon Go‘ das seltenste ist?", answer: "Unown", points: 400 },
-        { question: "Wie nennt man den sekundären Hauptcharakter auch?", answer: "Deuteragonist", points: 500 }
-    ],
-    Politik: [
-        { question: "Wer war der erste Präsident der Vereinigten Staaten?", answer: "George Washington", points: 100 },
-        { question: "Welches Land hat den ersten Schritt zu den Vereinten Nationen im Jahr 1945 gesetzt?", answer: "USA", points: 200 },
-        { question: "Welcher US-Präsident führte das Land durch die größten Teile des Zweiten Weltkriegs?", answer: "Franklin D. Roosevelt", points: 300 },
-        { question: "In welchem Jahr wurde die Berliner Mauer errichtet?", answer: "1961", points: 400 },
-        { question: "Wer war der erste weibliche Premierminister von Großbritannien?", answer: "Margaret Thatcher", points: 500 }
-    ],
-    Naturwissenschaften: [
-        { question: "Was ist der chemische Name für Wasser?", answer: "H2O", points: 100 },
-        { question: "Welches Element hat das chemische Symbol Fe?", answer: "Eisen", points: 200 },
-        { question: "Wie nennt man den Prozess, bei dem Pflanzen Sonnenlicht in Energie umwandeln?", answer: "Photosynthese", points: 300 },
-        { question: "Wie viele Chromosomen hat der Mensch?", answer: "46", points: 400 },
-        { question: "Wie nennt man den Vorgang, bei dem Wasser von einem festen in einen gasförmigen Zustand übergeht?", answer: "Sublimation", points: 500 }
-    ],
-    Allgemeinwissen: [
-        { question: "Wie viele Planeten gibt es in unserem Sonnensystem?", answer: "8", points: 100 },
-        { question: "Wie heißt die Währung in Japan?", answer: "Yen", points: 200 },
-        { question: "Was ist das viertgrößte Land der Welt?", answer: "China", points: 300 },
-        { question: "Wer erfand das Telefon?", answer: "Alexander Graham Bell", points: 400 },
-        { question: "Wer hat Uranium entdeckt?", answer: "Martin Heinrich Klaproth", points: 500 }
-    ],
-    Sport: [
-        { question: "Wie viele Spieler befinden sich in einer Fußballmannschaft auf dem Spielfeld?", answer: "11", points: 100 },
-        { question: "In welchem Jahr wurde die erste Fußball-Weltmeisterschaft ausgetragen?", answer: "1930", points: 200 },
-        { question: "Wie viele Grand-Slam-Turniere gibt es im Tennis?", answer: "4", points: 300 },
-        { question: "Welches Land hat bei den Olympischen Spielen 2008 in Peking die meisten Goldmedaillen gewonnen?", answer: "China", points: 400 },
-        { question: "Wie lange dauert ein Rugby-Spiel?", answer: "80 Minuten", points: 500 }
-    ],
-    GirlStuff: [
-        { question: "Welches Modehaus ist bekannt für das berühmte ‚Chanel No. 5‘ Parfüm?", answer: "Chanel", points: 100 },
-        { question: "Welche Frucht wird in der Beautybranche oft als 'Frucht der Liebe' bezeichnet?", answer: "Granatapfel", points: 200 },
-        { question: "Wie viele Schichten hat eine klassische französische Torte ‚Millefeuille‘?", answer: "3", points: 300 },
-        { question: "Was ist die teuerste und wirkungsvollste Creme der Welt?", answer: "N°1 von Med Beauty Swiss", points: 400 },
-        { question: "Was ist der Begriff für das Symptom des Haarausfalls bei Frauen nach der Geburt?", answer: "Postpartale Alopezie", points: 500 }
-    ],
-    SerienFilme: [
-        { question: "Wer spielte die Hauptrolle in dem Film ‚Titanic‘?", answer: "Leonardo DiCaprio", points: 100 },
-        { question: "Welcher Schauspieler spielte ‚Iron Man‘ im Marvel Cinematic Universe?", answer: "Robert Downey Jr.", points: 200 },
-        { question: "Welche Serie handelt von einem ehemaligen Lehrer, der Drogen herstellt?", answer: "Breaking Bad", points: 300 },
-        { question: "Wer spielte die Rolle des Joker im Film ‚The Dark Knight‘?", answer: "Heath Ledger", points: 400 },
-        { question: "In welchem Jahr wurde der erste ‚Star Wars‘ Film veröffentlicht?", answer: "1977", points: 500 }
-    ],
-    OnlineGames: [
-        { question: "In welchem Online-Spiel kämpfen zwei Teams aus je fünf Spielern gegeneinander, wobei ein Team die Bombe platziert und das andere sie entschärfen muss?", answer: "Counter-Strike: Global Offensive", points: 100 },
-        { question: "Welches beliebte Online-Spiel hat einen Modus namens ‚Heist‘, bei dem Teams versuchen, ein Bankkonto zu plündern?", answer: "Payday 2", points: 200 },
-        { question: "In welchem Jahr erschien der Champion \"Kalista\" bei League of Legends?", answer: "2014", points: 300 },
-        { question: "In welchem Spiel müssen die Spieler eine Insel überleben, die von wilden Tieren und gefährlichen Bedingungen geprägt ist?", answer: "ARK: Survival Evolved", points: 400 },
-        { question: "In welchem Spiel können Spieler in einer offenen Welt Zombies bekämpfen und dabei ein eigenes Fahrzeug steuern?", answer: "DayZ", points: 500 }
-    ],
-    Weltgeschichte: [
-        { question: "Welcher berühmte Feldherr war für die Eroberung des antiken Roms verantwortlich?", answer: "Julius Caesar", points: 100 },
-        { question: "Welche Zivilisation war für den Bau der Pyramiden verantwortlich?", answer: "Ägypter", points: 200 },
-        { question: "In welchem Jahr begann der kalte Weltkrieg?", answer: "1947", points: 300 },
-        { question: "Was war der Hauptauslöser des Zweiten Weltkriegs?", answer: "Der Überfall auf Polen durch Deutschland", points: 400 },
-        { question: "Wer war der letzte Kaiser des Heiligen Römischen Reiches, dessen Absetzung das Ende des Reiches markierte?", answer: "Franz II", points: 500 }
-    ],
-    Rätsel: [
-        { question: "Welcher Monat hat nur 28 Tage?", answer: "Alle Monate", points: 100 },
-        { question: "Ich kann alles essen, aber selbst nie etwas trinken. Was bin ich?", answer: "Feuer", points: 200 },
-        { question: "Ich habe Flügel, aber keine Federn. Was bin ich?", answer: "Ein Flugzeug", points: 300 },
-        { question: "Welches Wort wird immer kürzer, je länger es geschrieben wird?", answer: "Kürzer", points: 400 },
-        { question: "Was kommt einmal in einer Minute, zweimal in einem Moment, aber niemals in tausend Jahren?", answer: "Der Buchstabe ‚M‘", points: 500 }
-    ]
+    Kulinarik: [{
+        question: "Welches Land hat die höchste Produktion von Olivenöl weltweit?",
+        answer: "Spanien",
+        points: 100
+    }, {
+        question: "Wie nennt man die französische Technik, bei der man Fleisch in Rotwein kocht?",
+        answer: "Boeuf Bourguignon",
+        points: 200
+    }, {
+        question: "Was ist der Hauptbestandteil des traditionellen polnischen Eintopfs ‚Borschtsch‘?",
+        answer: "Rote Beete",
+        points: 300
+    }, {
+        question: "Welches Gewürz wird oft in indischer Küche verwendet und ist bekannt für seine gelbe Farbe?",
+        answer: "Kurkuma",
+        points: 400
+    }, {
+        question: "Welche Art von Weizen wird für die Herstellung von Pasta verwendet?",
+        answer: "Durum-Weizen",
+        points: 500
+    }],
+    Geografie: [{
+        question: "Welcher ist der größte Wüstenstaat der Welt?",
+        answer: "Algerien",
+        points: 100
+    }, {
+        question: "Wie heißt die Hauptstadt von Kirgisistan?",
+        answer: "Bischkek",
+        points: 200
+    }, {
+        question: "Welcher Fluss ist der längste in Europa?",
+        answer: "Die Wolga",
+        points: 300
+    }, {
+        question: "Welche Insel ist die größte im Mittelmeer?",
+        answer: "Sizilien",
+        points: 400
+    }, {
+        question: "Welches Land hat die meisten Vulkane der Welt?",
+        answer: "Indonesien",
+        points: 500
+    }],
+    Anime: [{
+        question: "In welchem Anime kämpfen die Hauptfiguren in einem Spiel, das ihre physischen und geistigen Fähigkeiten auf die Probe stellt?",
+        answer: "Sword Art Online",
+        points: 100
+    }, {
+        question: "In welchem Anime erlangen die Charaktere übernatürliche Kräfte durch das Essen von ‚Devil Fruits‘?",
+        answer: "One Piece",
+        points: 200
+    }, {
+        question: "Welcher Anime basiert auf der Geschichte von 12 Zodiac-Tieren, die in einem Wettbewerb gegeneinander antreten?",
+        answer: "Fruits Basket",
+        points: 300
+    }, {
+        question: "In welchem Anime geht es um den letzten Überlebenden eines mächtigen Magierclans, der seine Kräfte zurückerlangen muss?",
+        answer: "Fairy Tail",
+        points: 400
+    }, {
+        question: "Welche Anime-Serie wurde von Hayao Miyazaki produziert und behandelt den Konflikt zwischen Menschen und Natur?",
+        answer: "Prinzessin Mononoke",
+        points: 500
+    }],
+    Politik: [{
+        question: "Welches Land war das erste, das den Frauen das Wahlrecht gewährte?",
+        answer: "Neuseeland",
+        points: 100
+    }, {
+        question: "Wie heißt die Präsidentin von der Republik Indien?",
+        answer: "Draupadi Murmu",
+        points: 200
+    }, {
+        question: "In welchem Jahr wurde die europäische Wirtschafts- und Währungsunion (WWU) gegründet?",
+        answer: "1999",
+        points: 300
+    }, {
+        question: "Welches Land hat die längste autoritäre Herrschaft in der modernen Geschichte?",
+        answer: "Nordkorea",
+        points: 400
+    }, {
+        question: "Welche politische Bewegung wurde 1917 nach der Oktoberrevolution in Russland zur dominierenden Macht?",
+        answer: "Bolschewismus",
+        points: 500
+    }],
+    Naturwissenschaften: [{
+        question: "Welche Theorie beschreibt, dass die Gravitationskraft nicht durch ein klassisches Feld, sondern durch die Krümmung der Raumzeit vermittelt wird?",
+        answer: "Allgemeine Relativitätstheorie",
+        points: 100
+    }, {
+        question: "Was ist die Ursache des Phänomens der Polarlichter?",
+        answer: "Solare Winde, die auf das Magnetfeld der Erde treffen",
+        points: 200
+    }, {
+        question: "Was bezeichnet den Prozess, bei dem ein Molekül ein Elektron an ein anderes abgibt?",
+        answer: "Oxidation",
+        points: 300
+    }, {
+        question: "Welches Element hat die höchste Elektronegativität?",
+        answer: "Fluor",
+        points: 400
+    }, {
+        question: "Was ist das chemische Element, das bei Raumtemperatur als Gas vorliegt und den höchsten Atomradius hat?",
+        answer: "Cesium",
+        points: 500
+    }],
+    Allgemeinwissen: [{
+        question: "Welches Land besitzt die meisten Nobelpreisträger in der Geschichte?",
+        answer: "USA",
+        points: 100
+    }, {
+        question: "Wie heißt der einzige Planet, der nicht nach einer römischen Gottheit benannt wurde?",
+        answer: "Erde",
+        points: 200
+    }, {
+        question: "Was ist das einzige Land, das sowohl auf der südlichen als auch auf der nördlichen Hemisphäre liegt?",
+        answer: "Ecuador",
+        points: 300
+    }, {
+        question: "Welches Land hat das höchste Bruttoinlandsprodukt (BIP) pro Kopf weltweit?",
+        answer: "Luxemburg",
+        points: 400
+    }, {
+        question: "Wie nennt man die Wissenschaft von den Fischen?",
+        answer: "Ichthyologie",
+        points: 500
+    }],
+    Sport: [{
+        question: "Welche Nation gewann die erste FIFA-Weltmeisterschaft im Jahr 1930?",
+        answer: "Uruguay",
+        points: 100
+    }, {
+        question: "Wie viele Goldmedaillen holte Michael Phelps insgesamt bei den Olympischen Spielen?",
+        answer: "23",
+        points: 200
+    }, {
+        question: "Welches Land ist Gastgeber der Olympischen Winterspiele 2026?",
+        answer: "Italien",
+        points: 300
+    }, {
+        question: "Welcher Sport wurde erstmals 1896 bei den Olympischen Spielen eingeführt?",
+        answer: "Basketball",
+        points: 400
+    }, {
+        question: "Wie viele Jahre dauerte die ursprüngliche Tour de France im Jahr 1903?",
+        answer: "19 Tage",
+        points: 500
+    }],
+    GirlStuff: [{
+        question: "Welches Modehaus ist bekannt für seine berühmte ‚New Look‘-Kollektion aus dem Jahr 1947?",
+        answer: "Christian Dior",
+        points: 100
+    }, {
+        question: "Welche berühmte Marke wurde 1949 von Coco Chanel gegründet?",
+        answer: "Chanel",
+        points: 200
+    }, {
+        question: "Was war das erste Make-up-Produkt, das von Estée Lauder auf den Markt gebracht wurde?",
+        answer: "Cleansing Cream",
+        points: 300
+    }, {
+        question: "Welches Parfüm aus dem Jahr 1921 gilt als das berühmteste der Welt?",
+        answer: "Chanel No. 5",
+        points: 400
+    }, {
+        question: "Welcher Designer kreierte die berühmte ‚Conical Bra‘, die Madonna in den 1990er Jahren trug?",
+        answer: "Jean-Paul Gaultier",
+        points: 500
+    }],
+    SerienFilme: [{
+        question: "Wer spielte die Hauptrolle in ‚The Godfather‘?",
+        answer: "Marlon Brando",
+        points: 100
+    }, {
+        question: "In welchem Film tritt ein Dinosaurier als wichtiger Charakter auf und wird von Jeff Goldblum gespielt?",
+        answer: "Jurassic Park",
+        points: 200
+    }, {
+        question: "Wer führte Regie bei ‚Pulp Fiction‘?",
+        answer: "Quentin Tarantino",
+        points: 300
+    }, {
+        question: "Welcher Schauspieler spielte die Rolle des Jack Dawson in ‚Titanic‘?",
+        answer: "Leonardo DiCaprio",
+        points: 400
+    }, {
+        question: "In welchem Film wird der Charakter ‚The Joker‘ von Heath Ledger verkörpert?",
+        answer: "The Dark Knight",
+        points: 500
+    }],
+    OnlineGames: [{
+        question: "In welchem Spiel müssen die Spieler im 5v5-Modus in einer Arena kämpfen?",
+        answer: "League of Legends",
+        points: 100
+    }, {
+        question: "Welches Spiel hat den 'Battle Royale' Modus populär gemacht?",
+        answer: "PlayerUnknown's Battlegrounds",
+        points: 200
+    }, {
+        question: "Welches Online-Spiel aus dem Jahr 2004 ist bekannt für seine riesigen Welten und seine umfangreiche Lore?",
+        answer: "World of Warcraft",
+        points: 300
+    }, {
+        question: "Welches Spiel ist für seine riesige offene Welt und den Einsatz von Magie bekannt?",
+        answer: "The Elder Scrolls V: Skyrim",
+        points: 400
+    }, {
+        question: "Welches Spiel basiert auf einer Serie von rätselhaften Verbrechen und Detektivarbeit?",
+        answer: "L.A. Noire",
+        points: 500
+    }],
+    Weltgeschichte: [{
+        question: "Welches antike Reich war bekannt für die Pyramiden und Sphinx?",
+        answer: "Ägypten",
+        points: 100
+    }, {
+        question: "Was war der Hauptgrund für den Ausbruch des Ersten Weltkriegs?",
+        answer: "Das Attentat auf Erzherzog Franz Ferdinand",
+        points: 200
+    }, {
+        question: "Welcher deutsche Kaiser führte das Deutsche Reich in den Ersten Weltkrieg?",
+        answer: "Wilhelm II.",
+        points: 300
+    }, {
+        question: "Wer war der Hauptarchitekt der Französischen Revolution?",
+        answer: "Maximilien Robespierre",
+        points: 400
+    }, {
+        question: "Welcher Historiker prägte die Theorie des ‚Endes der Geschichte‘ nach dem Kalten Krieg?",
+        answer: "Francis Fukuyama",
+        points: 500
+    }],
+    Rätsel: [{
+        question: "Ich kann alles erzählen, aber nie sprechen. Was bin ich?",
+        answer: "Ein Buch",
+        points: 100
+    }, {
+        question: "Was ist immer vor dir, aber kann niemals gesehen werden?",
+        answer: "Die Zukunft",
+        points: 200
+    }, {
+        question: "Je mehr du wegnimmst, desto größer wird es. Was ist das?",
+        answer: "Ein Loch",
+        points: 300
+    }, {
+        question: "Welche Zahl folgt diesem Muster? 1, 4, 9, 16, ___",
+        answer: "25",
+        points: 400
+    }, {
+        question: "Ich habe eine Schulter, aber keinen Kopf. Was bin ich?",
+        answer: "Ein Anzug",
+        points: 500
+    }]
 };
